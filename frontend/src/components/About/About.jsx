f5ab4ab26e7f668561d9366c2bd1207aa3474894
h0uu0h <houjq777@gmail.com>
2025-11-20 22:50:31 +0800

    11.20_dockerfile change

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -7,11 +7,12 @@ const About = () => {
         <div className="about">
             <div className="about-container">
                 <div className="about-info">
-                    <h1>侯家祺</h1>
+                    <h1>侯家祺 </h1>
                     <div className="about-meta">
                         <div className="about-m">Interactive Design, Graphic Design</div>
-                        <div style={{ whiteSpace: "pre-line",fontWeight:"300" }}>{"北京邮电大学 2020 级本科生\n北京邮电大学 2024 级硕士在读"}</div>
+                        <div style={{ whiteSpace: "pre-line", fontWeight: "300" }}>{"北京邮电大学 2020 级本科生\n北京邮电大学 2024 级硕士在读"}</div>
                         <div className="about-m">{"PS, AI, AE, PR\nBlender, Unity, C#\nPython, React, JavaScript, HTML, CSS"}</div>
+                        <img src="/hjq.JPG" alt="hjq" style={{ width: "clamp(36*3px, 24vw, 84*3px)" }} />
                         <MagicLink href="mailto:1978708083@qq.com">1978708083@qq.com</MagicLink>
                     </div>
                 </div>
